d13cab478ac3a3cc294d32d1c0ab5b7400581e1c
ZajRJ <zajrj.3@gmail.comn>
2023-05-12 22:36:06 -0400

    REFACTOR: formatted

--- taller_2.c
+++ taller_2.c
@@ -16,16 +16,14 @@ int main()
 
     fillMatrix(data, &indexes);
 
-    for (x = 0; x < indexes-1; x++)
+    for (x = 0; x < indexes - 1; x++)
     {
-        for ( y = 0; y < 3; y++)
+        for (y = 0; y < 3; y++)
         {
-            printf(" [%.1f] ",data[x][y]);
+            printf(" [%.1f] ", data[x][y]);
         }
         printf("\n");
-        
     }
-    
 
     totalCat = calculateTotal(data, 1, indexes);
     totalCat_2 = calculateTotal(data, 2, indexes);
@@ -36,23 +34,22 @@ int main()
     printf("\nVentas totales: %.02f\nVentas categoria 1: %.02f\nVentas categoria 2: %.02f\nVentas categoria 3: %.02f", totalSales, totalCat, totalCat_2, totalCat_3);
 
     output = fopen("ventas_totales.txt", "w");
-    
+
     fprintf(output, "ventas_totales.txt\n\nVentas totales: %.02f\nVentas categoria 1: %.02f\nVentas categoria 2: %.02f\nVentas categoria 3: %.02f", totalSales, totalCat, totalCat_2, totalCat_3);
-    
+
     fclose(output);
 
     printf("\n\nSe ha generdo un archivo con los datos de ventas.\n\nEl programa se cerrara automaticamente en 5 segundos ");
     sleep(5);
 
     return 0;
-
 }
 
 void fillMatrix(float data[MAX_SIZE][3], int *indexes)
 {
     int category, soldQuantity;
     float unitPrice;
-    FILE *file = fopen("ventas.txt","r");
+    FILE *file = fopen("ventas.txt", "r");
 
     if (file == NULL)
     {
@@ -62,7 +59,7 @@ void fillMatrix(float data[MAX_SIZE][3], int *indexes)
 
     while (!feof(file) && *indexes < MAX_SIZE)
     {
-        fscanf(file,"%i,%i,%f", &category, &soldQuantity, &unitPrice);
+        fscanf(file, "%i,%i,%f", &category, &soldQuantity, &unitPrice);
 
         data[*indexes][0] = category;
         data[*indexes][1] = soldQuantity;
@@ -71,7 +68,6 @@ void fillMatrix(float data[MAX_SIZE][3], int *indexes)
         *indexes = *indexes + 1;
     }
     fclose(file);
-
 }
 
 float calculateTotal(float data[MAX_SIZE][3], int category, int indexes)
@@ -82,7 +78,7 @@ float calculateTotal(float data[MAX_SIZE][3], int category, int indexes)
     {
         if (data[i][0] == category)
         {
-            total = total + ((data[i][1]+0.0) * data[i][2]);
+            total = total + ((data[i][1] + 0.0) * data[i][2]);
         }
     }
     return total;
